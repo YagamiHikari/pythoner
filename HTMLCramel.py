1c9628877665d2b176f20cd4651f189d81573ff0
zhangbc <zhangbocheng189@163.com>
2015-05-20 14:24:11 +0800

    HTMLCramel.py
    抓取一组网页数据，每个网页生成一天记录，先汇总为一个txt文本，然后转换成一张Excel。

---
+++ HTMLCramel.py
@@ -0,0 +1,42 @@
+# -*- coding:UTF-8 -*-
+import os
+import os.path
+import re
+import xlwt
+from xlrd import open_workbook
+from xlutils.copy import copy
+import time
+
+def saveTxt(text,data):
+    fw=open(text,'a')
+    strTxt=""
+    for txt in data:
+        strTxt=strTxt+('%30s\t')%txt
+    fw.write(strTxt+'\n')
+    fw.close()
+
+def txtToExcel(text,excel,sheet):
+    data=open(text).read()
+    lines=data.split('\n')
+    wb=xlwt.Workbook()
+    ws=wb.add_sheet(sheet)
+    dataStr=''
+    for i in range(0,len(lines)):
+        fields=lines[i].split('\t')
+        for j in range(0,len(fields)):
+            ws.write(i,j,fields[j].decode('UTF-8').strip())
+    wb.save(excel)
+    
+def MemberData(path):
+    rex='>([^<]*)</span></td>'
+    for fname in os.listdir(path):
+        data=open(path+'\\'+fname,'r').read()
+        dataResult=re.findall(rex,data)
+        saveTxt('user02.txt',dataResult)
+    txtToExcel('user02.txt','user02.xls','data')
+    
+if __name__=='__main__':
+    startTime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()) 
+    MemberData(r'E:\Python\MayTask\taskData\user')
+    EndTime=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
+    print 'Begin:'+startTime,'\nEnd:'+EndTime
